be9ede2e16cba49caa771f727dc7e8366328c35b
KJane0120 <rlawoals5755@gmail.com>
2025-03-24 01:38:10 +0900

    [Feat] Canvas, Scripts 생성

---
+++ Assets/02. Scripts/UIMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI goldText;
+    private int gold; 
+
+    private void Start()
+    {
+        gold = 20000;
+        goldText.text = string.Format("{0:N0}", gold);
+    }
+
+}
